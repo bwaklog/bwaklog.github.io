3c645f9c47a84cf826a76504187efe2adee79784
bwaklog <aditya.mh@outlook.com>
2023-11-22 03:18:22 +0530

    smthn

--- js/app.js
+++ js/app.js
@@ -15,5 +15,17 @@ function shuffleText() {
     paragraph.textContent = characters.join(' ');
 }
 
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('mousemove', function (e) {
+        const x = e.clientX / window.innerWidth * 100;
+        const y = e.clientY / window.innerHeight * 100;
+        console.log(x, y)
+        console.log(document.body.style.backgroundPosition);
+        document.body.style.background = `radial-gradient(circle at ${x}% ${y}%, #280071, #1f0000)`;
+    });
+});
+
+
 // Update the text every 1000 milliseconds (1 second)
 setInterval(shuffleText, 50);
