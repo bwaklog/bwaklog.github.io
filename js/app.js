d478844b5a6a35c5d0a71219c92cb9d908a2c78c
bwaklog <aditya.mh@outlook.com>
2023-12-09 20:36:44 +0530

    new redirect

--- js/app.js
+++ js/app.js
@@ -15,9 +15,9 @@ document.addEventListener('DOMContentLoaded', function () {
     document.addEventListener('mousemove', function (e) {
         const x = e.clientX / window.innerWidth * 100;
         const y = e.clientY / window.innerHeight * 100;
-        console.log(x, y)
-        console.log(document.body.style.backgroundPosition);
-        document.body.style.background = `radial-gradient(circle at ${x}% ${y}%, #280071, #1f0000)`;
+        // console.log(x, y)
+        // console.log(document.body.style.backgroundPosition);
+        // document.body.style.background = `radial-gradient(circle at ${x}% ${y}%, #280071, #1f0000)`;
     });
 });
 
